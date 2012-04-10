634dc02a08b238fb32b4b6fdda29aadf1628c366
Nico Hagenburger <nico@hagenburger.net>
2012-04-10 18:14:24 +0200

    initial commit

---
+++ lib/compass-placeholders.rb
@@ -0,0 +1,7 @@
+require "compass-placeholders/version"
+
+module Compass
+  module Placeholders
+    # Your code goes here...
+  end
+end
---
+++ lib/compass-placeholders/version.rb
@@ -0,0 +1,5 @@
+module Compass
+  module Placeholders
+    VERSION = "0.0.1"
+  end
+end
